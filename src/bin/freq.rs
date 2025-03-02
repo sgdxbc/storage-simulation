48ffc97608a7e72229cad328a9647d91ec23da33
sgdxbc <sung@comp.nus.edu.sg>
2025-03-02 08:12:56 +0000

    Update freq simulation

--- src/bin/freq.rs
+++ src/bin/freq.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    fmt::Write as _,
     fs::{File, create_dir_all},
     io::Write,
     time::UNIX_EPOCH,
@@ -12,7 +13,6 @@ use storage_simulation::{Classified, Overlay};
 
 fn main() -> anyhow::Result<()> {
     let num_find: u32 = 1_000_000;
-    let num_build = 1_000;
     let find_size: usize = 3;
 
     let mut rng = rng();
@@ -23,37 +23,49 @@ fn main() -> anyhow::Result<()> {
     ))?;
     writeln!(
         output,
-        "num_node,num_build,build_i,num_find,find_size,class_begin,class_end,skew,class,num_class_node,hit_count",
+        "num_node,num_find,find_size,num_class,skew,class,num_class_node,hit_count",
     )?;
-    for num_node in [10_000, 20_000, 30_000, 40_000, 50_000] {
-        run(
-            num_node,
-            num_build,
-            num_find,
-            find_size,
-            8,
-            24,
-            0.,
-            StdRng::from_rng(&mut rng),
-            &mut output,
-        )?
-    }
+    // let num_build = 1_000;
+    // for num_node in [10_000, 20_000, 30_000, 40_000, 50_000] {
+    //     run(
+    //         num_node,
+    //         num_build,
+    //         num_find,
+    //         find_size,
+    //         8,
+    //         24,
+    //         0.,
+    //         StdRng::from_rng(&mut rng),
+    //         &mut output,
+    //     )?
+    // }
+    let num_node = 10_000;
+    run(
+        1,
+        num_node,
+        num_find,
+        find_size,
+        8,
+        1.,
+        StdRng::from_rng(&mut rng),
+        &mut output,
+    )?;
+
     Ok(())
 }
 
 fn run(
-    num_node: usize,
     num_build: u32,
+    num_node: usize,
     num_find: u32,
     find_size: usize,
-    class_begin: u8,
-    class_end: u8,
+    num_class: u8,
     skew: f32,
     mut rng: impl Rng,
     mut output: impl Write,
 ) -> anyhow::Result<()> {
-    eprintln!("Number of node {num_node}");
-    let class_distr = Zipf::new((class_end - class_begin) as f32, skew)?;
+    eprintln!("Number of node {num_node} Number of class {num_class} Skew {skew}");
+    let capacity_distr = Zipf::new((1usize << (num_class - 1)) as f32, skew)?;
     let results = (0..num_build)
         .map(|i| (i, StdRng::from_rng(&mut rng)))
         .collect::<Vec<_>>()
@@ -61,43 +73,38 @@ fn run(
         .map(move |(build_i, mut rng)| {
             let mut network = Classified::new();
             let mut node_classes = HashMap::new();
-            let mut num_class_node = vec![0; 64];
+            let mut num_class_node = vec![0; num_class as _];
             for _ in 0..num_node {
                 let node_id = rng.random();
-                let class = class_end - class_distr.sample(&mut rng) as u8;
+                let capacity = capacity_distr.sample(&mut rng);
+                let class = capacity.log2().floor() as _;
                 network.insert_node(node_id, class);
                 node_classes.insert(node_id, class);
                 num_class_node[class as usize] += 1;
             }
-            let mut freqs = vec![0u32; 64];
+            let mut freqs = vec![0u32; num_class as _];
             for _ in 0..num_find {
                 let node_ids = network.find(rng.random(), find_size);
                 for node_id in node_ids {
                     freqs[node_classes[&node_id] as usize] += 1
                 }
             }
-            // if last_report.elapsed() >= Duration::from_secs(1) {
-            //     println!("{build_i:03}/{num_build:03} Number of node {num_node}");
-            //     last_report = Instant::now()
-            // }
-            // println!(
-            //     "{class_begin} {:?}",
-            //     &freqs[class_begin as usize..class_end as usize]
-            // );
             eprint!("\r{:80}", format!("{build_i}"));
-            (build_i, num_class_node, freqs)
+            let mut lines = String::new();
+            for class in 0..num_class {
+                writeln!(
+                    &mut lines,
+                    "{num_node},{num_find},{find_size},{num_class},{skew},{class},{},{}",
+                    num_class_node[class as usize], freqs[class as usize]
+                )
+                .unwrap()
+            }
+            lines
         })
         .collect::<Vec<_>>();
     eprintln!();
-    for (build_i, num_class_node, freqs) in results {
-        for class in class_begin..class_end {
-            writeln!(
-                output,
-                "{num_node},{num_build},{build_i},{num_find},{find_size},{class_begin},{class_end},{skew},{class},{},{}",
-                num_class_node[class as usize], freqs[class as usize]
-            )?
-        }
+    for result in results {
+        write!(&mut output, "{result}")?
     }
-
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -63,24 +63,24 @@ pub mod classified {
     pub type NodeId = (super::NodeId, u8);
 
     pub fn subnet(node_id: super::NodeId, class: u8) -> usize {
-        if class == 0 {
+        if class >= super::SUBNET_PROXIMITY as u8 {
             0
         } else {
-            (node_id >> (super::NodeId::BITS - class as u32)) as _
+            (node_id >> (super::NodeId::BITS - super::SUBNET_PROXIMITY + class as u32)) as _
         }
     }
 
     pub fn distance(node_id: super::NodeId, target: super::DataId, class: u8) -> super::Distance {
-        (node_id ^ target) & !(!0 >> class)
+        (node_id ^ target) & (!0 >> class)
     }
 }
 
 pub struct Classified {
-    // class SUBNET_PROXIMITY and above
-    subnets: Vec<Vec<classified::NodeId>>,
     // class 0..SUBNET_PROXIMITY
     // [class -> [subnet prefix -> [node id]]]
-    classified_subnets: Vec<Vec<Vec<NodeId>>>,
+    subnets: Vec<Vec<Vec<NodeId>>>,
+    // class SUBNET_PROXIMITY and above
+    unclassified: Vec<classified::NodeId>,
 }
 
 impl Default for Classified {
@@ -92,61 +92,43 @@ impl Default for Classified {
 impl Classified {
     pub fn new() -> Self {
         Self {
-            subnets: (0..1 << SUBNET_PROXIMITY)
-                .map(|_| Default::default())
+            subnets: (0..SUBNET_PROXIMITY)
+                .map(|class| {
+                    (0..1 << (SUBNET_PROXIMITY - class))
+                        .map(|_| Default::default())
+                        .collect()
+                })
                 .collect(),
-            classified_subnets: (0..SUBNET_PROXIMITY)
-                .map(|class| (0..1 << class).map(|_| Default::default()).collect())
-                .collect(),
+            unclassified: Default::default(),
         }
     }
 
     pub fn insert_node(&mut self, node_id: NodeId, class: u8) {
         if class >= SUBNET_PROXIMITY as u8 {
-            self.subnets[(node_id >> (NodeId::BITS - SUBNET_PROXIMITY)) as usize]
-                .push((node_id, class))
+            self.unclassified.push((node_id, class))
         } else {
-            self.classified_subnets[class as usize][classified::subnet(node_id, class)]
-                .push(node_id)
+            self.subnets[class as usize][classified::subnet(node_id, class)].push(node_id)
         }
     }
 
     fn find(&self, data_id: DataId, count: usize) -> Vec<NodeId> {
         let data_subnet = data_id >> (DataId::BITS - SUBNET_PROXIMITY);
-        let mut node_ids = Vec::new();
+        let mut node_ids = self.unclassified.clone();
         for diff in 0.. {
             let subnet_index = (data_subnet ^ diff) as usize;
             for class in 0..SUBNET_PROXIMITY {
-                let mut subnet = self.classified_subnets[class as usize]
-                    [subnet_index >> (SUBNET_PROXIMITY - class)]
-                    .clone();
-                if subnet.len() <= count - node_ids.len() {
-                    node_ids.extend(subnet);
-                } else {
-                    subnet
-                        .sort_unstable_by_key(|&id| classified::distance(id, data_id, class as _));
-                    node_ids.extend(subnet.into_iter().take(count - node_ids.len()));
-                    break;
-                }
-            }
-            let mut subnet = self.subnets[subnet_index].clone();
-            if subnet.len() <= count - node_ids.len() {
-                node_ids.extend(subnet.into_iter().map(|(id, _)| id))
-            } else if node_ids.len() < count {
-                subnet
-                    .sort_unstable_by_key(|&(id, class)| classified::distance(id, data_id, class));
                 node_ids.extend(
-                    subnet
-                        .into_iter()
-                        .map(|(id, _)| id)
-                        .take(count - node_ids.len()),
+                    self.subnets[class as usize][subnet_index >> class]
+                        .iter()
+                        .map(|&id| (id, class as _)),
                 )
             }
-            if node_ids.len() == count {
+            if node_ids.len() >= count {
                 break;
             }
         }
-        node_ids
+        node_ids.sort_unstable_by_key(|&(id, class)| classified::distance(id, data_id, class));
+        node_ids.into_iter().map(|(id, _)| id).collect()
     }
 }
 
